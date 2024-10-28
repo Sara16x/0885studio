9e3bac10c94c84e45d2ab958a988edeae58e775f
Sara16x <sara.cannone77@gmail.com>
2024-10-28 13:34:17 +0100

    Imperfection

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-var multipleCardCarousel = document.querySelector(
-  "#carouselExampleControls"
-);
-if (window.matchMedia("(min-width: 768px)").matches) {
-  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-    interval: false,
-  });
-  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-  var cardWidth = $(".carousel-item").width();
-  var scrollPosition = 0;
-  $("#carouselExampleControls .carousel-control-next").on("click", function () {
-    if (scrollPosition < carouselWidth - cardWidth * 4) {
-      scrollPosition += cardWidth;
-      $("#carouselExampleControls .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
-      $("#carouselExampleControls .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-} else {
-  $(multipleCardCarousel).addClass("slide");
-}
-
-
-
 // Disabilita il tasto destro del mouse
 window.addEventListener('contextmenu', function (e) {
 e.preventDefault();
@@ -40,3 +6,9 @@ e.preventDefault();
 // Disabilita la selezione del testo
 document.body.style.webkitUserSelect='none'; //Chrome, Opera
 document.body.style.userSelect='none'; //Rest
+
+// Sovrascrivi window.open per evitare l'apertura di nuove finestre
+window.open = function() {
+    console.log("L'apertura di nuove finestre è stata disabilitata.");
+    return null; // Non apre nulla
+};
